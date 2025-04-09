22d51caca1a9162c5271d488a939cb164f0cd52f
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-04-09 16:33:33 +0200

    7t

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,12 @@
+#!/bin/usr/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    tp = [0,0,0,0]
+    i = 0
+    for nt in tuple_a:
+        tp[i] = nt
+        i += 1
+    p = 2
+    for nj in tuple_b:
+        tp[p] = nj
+        p += 1
+    return (tp[0] + tp[2], tp[1] + tp[3])

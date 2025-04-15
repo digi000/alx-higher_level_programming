2631edd1e9958bb7eb1e43ea68646eb9d91c950b
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-04-15 22:36:29 +0200

    1t

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""a class Square that defines a square"""
+class Square:
+    """a class Square that defines a square"""
+    def __init__(self, size):
+        """a class Square that defines a square"""
+        self.__size = size

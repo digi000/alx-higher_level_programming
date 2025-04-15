ece90be7cfce72e7943f7589c7c3d6a32f22747f
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-04-15 23:12:48 +0200

    3t

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class Square that defines a square"""
+class Square:
+    """class Square that defines a square"""
+    def __init__(self, size=0):
+        """size must be >= 0"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """ returns the current square area"""
+        return self.__size * self.__size

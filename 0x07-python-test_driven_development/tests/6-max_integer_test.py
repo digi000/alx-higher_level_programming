62c3d6c3f145e26787fdb187ced38f7caaef5b83
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-04-17 08:05:01 +0200

    5t-

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -6,6 +6,12 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(mi([1,2,3]), 3)
         self.assertEqual(mi([1,3,2]), 3)
         self.assertEqual(mi([]), None)
+        self.assertEqual(mi([-10, -20, -3, -40]), -3)
+        self.assertEqual(mi([22]), 22)
+        self.assertEqual(mi([5, 5, 5, 5]), 5)
+        self.assertEqual(mi([10, 2, 3, 1]), 10)
+        self.assertEqual(mi([1, 2, 3, 10]), 10)
+        self.assertRaises(TypeError,mi,[1, "a", 3])
 
 if __name__ == "__main__":
     unittest.main()

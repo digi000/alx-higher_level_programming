023a449a20fd2bb74aaa1bc66b24fc74bc906f11
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-04-17 16:48:02 +0200

    7t-

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -4,7 +4,6 @@ class Rectangle:
     """empty class Rectangle that defines a rectangle"""
     number_of_instances = 0
     print_symbol = '#'
-    print(print_symbol)
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height

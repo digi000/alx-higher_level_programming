64154cb1bf288bebc82d488a6d40b54c8640246d
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-04-19 17:20:55 +0200

    4t

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""if the object is an instance of a class that inherited"""
+def inherits_from(obj, a_class):
+    """if the object is an instance of a class that inherited"""
+    if isinstance(obj, a_class):
+        return True
+    return False

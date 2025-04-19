f7781edabaf1d64c08c9a55b6345d1f9fa55b8c5
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-04-19 16:33:10 +0200

    3t-

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""turns True if the object is an instance of, or if the obje"""
 def is_kind_of_class(obj, a_class):
+    """turns True if the object is an instance of, or if the obje"""
     if isinstance(obj, a_class):
         return True
     return False

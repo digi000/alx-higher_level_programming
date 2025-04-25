5b221fecaa0a4c327950d112fd0f81ec560b5920
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-04-25 20:36:26 +0200

    1t-

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,6 +2,6 @@
 """1. Write to a file"""
 def write_file(filename="", text=""):
     """1. Write to a file"""
-    with open(filename, 'w', 'UTF8') as f:
+    with open(filename, "w", encoding="UTF8") as f:
         nc = f.write(text)
         return nc

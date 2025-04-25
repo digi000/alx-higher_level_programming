3676fb5e29a3d0ca3ee6bff5aa64d79344d19098
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-04-25 21:09:22 +0200

    5t-

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -3,5 +3,5 @@
 import json
 def save_to_json_file(my_obj, filename):
     """def save_to_json_file(my_obj, filename):"""
-    with open(filename, 'w' encoding="UTF8") as f:
+    with open(filename, 'w', encoding="UTF8") as f:
         json.dump(my_obj, f)

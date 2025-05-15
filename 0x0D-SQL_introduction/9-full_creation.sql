856414c281c204c5496d97f0b88c89f4a3fc961a
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-05-15 16:28:03 +0200

    9t-

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- ct
-CREATE TABLE second_table(id INT,name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table(id INT,name VARCHAR(256), score INT);
 
 -- it
 INSERT INTO second_table VALUES

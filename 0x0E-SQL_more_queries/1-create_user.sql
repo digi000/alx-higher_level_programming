4c825b15f90388eaaf273fa2e4158fb63ecc7527
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-05-16 19:51:42 +0200

    1t1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,8 @@
 -- cu
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd' AND GRANTS ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- gp
+GRANTS ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- FP
+FLUSH PRIVILEGES;

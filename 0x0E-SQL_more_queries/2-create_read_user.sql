e6ec3538972ace904fa9d30c290000d71c4b4156
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-05-16 20:05:33 +0200

    2t

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- cu
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+
+--cu
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+
+-- gp
+GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';

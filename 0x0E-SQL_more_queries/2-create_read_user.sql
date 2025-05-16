1e001950461b2872386fa6a019ec214e75fb73b9
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-05-16 20:17:15 +0200

    2t1

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,9 +1,10 @@
 -- cu
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_2`;
 
---cu
+-- cu
 CREATE USER IF NOT EXISTS `user_0d_2`@`localhost` IDENTIFIED BY `user_0d_2_pwd`;
 
 -- gp
 GRANT SELECT ON `hbtn_0d_2`.* TO `user_0d_2`@`localhost`;
+-- fp
 FLUSH PRIVILEGES;

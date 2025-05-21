dea4716714b275edfb5e50e8c8f6003d0a3bbfea
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-05-21 20:27:15 +0200

    7t-

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 const lit = process.argv;
+const mk = "C is fun"
 if (lit[2]/2){
 	let i;
 	for(i = 0; i < parseInt(lit[2]); i++){
-		console.log("C is fun");
+		console.log(mk);
 	}
 } else {
 	console.log("Missing number of occurrences");

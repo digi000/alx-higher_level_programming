50f2c4c15a814352abadaad793eddf98fc44f751
Tuyisenge Jordan <jordanale.112@gmail.com>
2025-06-28 22:05:47 +0200

    0t

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+filePath = process.argv[2];
+
+const fs = require('fs');
+
+fs.readFile(filePath, 'utf8', (err, data) => {
+	if (err) {
+		console.error(err);
+		return;
+	}
+	console.log(data);
+})
